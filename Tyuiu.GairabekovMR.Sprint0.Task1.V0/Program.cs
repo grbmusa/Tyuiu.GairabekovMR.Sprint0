c80afca55ee32deadd68f4b0936136610dbf0680
grbmusa <gairabekovmusa@gmail.com>
2024-10-12 20:49:28 +0500

    Add files via upload

---
+++ Tyuiu.GairabekovMR.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Tyuiu.GairabekovMR.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            string name = "Муса";
+            int age = 20;
+            Console.WriteLine("Здравствуйте, меня зовут " + name + " мне " + age + " лет!");
+            Console.ReadKey();
+        }
+    }
+}
