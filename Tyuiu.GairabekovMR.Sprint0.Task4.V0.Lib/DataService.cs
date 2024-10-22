902b27795e3f63b332550b5cd6db6885de90fedc
TyuiuGairabekovMR@outlook.com <TyuiuGairabekovMR@outlook.com>
2024-10-22 22:53:29 +0500

    Task4

---
+++ Tyuiu.GairabekovMR.Sprint0.Task4.V0.Lib/DataService.cs
@@ -0,0 +1,32 @@
+﻿namespace Tyuiu.GairabekovMR.Sprint0.Task4.V0.Lib
+{
+    public class DataService
+    {
+        //Пример линейной структуры
+        public static int Addition(int a, int b)
+        { 
+            return a + b; 
+        }
+
+        //Пример линейной структуры
+        public static int Subtraction(int a, int b)
+        {
+            return a - b;
+        }
+
+        //Пример линейной структуры
+        public static int Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+
+
+        //Пример линейной структуры
+        public static double Devision(int a, int b)
+        {
+            return a / b;
+        }
+
+
+    }
+}
