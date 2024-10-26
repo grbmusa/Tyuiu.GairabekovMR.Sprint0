4187ee9e7f87f9ffbd66c4dbeb928bac1479e0a9
TyuiuGairabekovMR@outlook.com <TyuiuGairabekovMR@outlook.com>
2024-10-26 18:21:14 +0500

    Task7

---
+++ Tyuiu.GairabekovMR.Sprint0.Task7.V0.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+using Tyuiu.GairabekovMR.Sprint0.Task7.V0.Lib; 
+namespace Tyuiu.GairabekovMR.Sprint0.Task7.V0.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void CheckAdditionArrayValid()
+        {
+            int [] nums1 = new int [] { 1, 1, 1, 1, 1, };
+            int [] nums2 = new int [] { 1, 1, 1, 1, 1, };
+            int [] nums3 = new int [] { 2, 2, 2, 2, 2, };
+
+            int[] res = DataService.AdditionArrays(nums1, nums2);
+            CollectionAssert.AreEqual(nums3, res);
+
+        }
+    }
+}
